f0bcd86c198545f641645f189648bb2623fae3f3
vikram-mali <vikrammali2345@gmail.com>
2026-05-10 12:36:20 +0530

    Init Commit

---
+++ src/main/java/com/gridstore/huevista/HueVistaApplication.java
@@ -0,0 +1,13 @@
+package com.gridstore.huevista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HueVistaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HueVistaApplication.class, args);
+    }
+
+}
